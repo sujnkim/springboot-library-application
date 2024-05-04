fee7da54dd41589922c7f9f8f43a1a5dfc807300
sujnkim <blackout3210@naver.com>
2024-05-04 13:23:25 +0900

    refactor: User update API 역할 분리
    Layered Architecture MCV 패턴에 맞춰 Controller 역할 분리
    Controller - API의 진입지점, HTTP Body를 객체로 변환
    Service - 유저 유무를 확인하고 예외 처리
    Repository - SQL을 사용해 Database와 통신 담당
    
    JdbcTemplate를 UserRepository에 변수로 추가함
    UserRepository를 인스턴스화 시킬 때 생성자를 통해 JdbcTemplate를
    넣어주도록 변경

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,25 +1,23 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
+    private final UserService userService;
     private final JdbcTemplate jdbcTemplate;
 
     public UserController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        userService = new UserService(jdbcTemplate);
     }
 
     @PostMapping("/user")
@@ -51,19 +49,7 @@ public class UserController {
 
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-
-        // User 존재 여부 확인
-        String readSql = "SELECT * FROM user WHERE id = ?";
-        boolean isUserNotExist = jdbcTemplate
-                .query(readSql, (rs, rowNum) -> 0, request.getId())
-                .isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userService.updateUser(request);
     }
 
     @DeleteMapping("/user")
