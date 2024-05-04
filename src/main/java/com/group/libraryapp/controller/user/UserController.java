4d71f840974250f3bff11a2903e466ec3714f0d8
sujnkim <blackout3210@naver.com>
2024-05-04 13:49:19 +0900

    refactor: User create, update API Controller 역할 분리

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -20,30 +20,16 @@ public class UserController {
         userService = new UserService(jdbcTemplate);
     }
 
+
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        //users.add(new User(request.getName(), request.getAge()));
-
-        String sql = "INSERT INTO user(name, age) VALUES(?,?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
-
+        userService.saveUser(request);
     }
+
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-//        List<UserResponse> responses = new ArrayList<>();
-//        for (int i = 0; i < users.size(); i++) {
-//            responses.add(new UserResponse(i + 1, users.get(i)));
-//        }
-//        return responses;
-
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
     }
 
 
@@ -51,6 +37,7 @@ public class UserController {
     public void updateUser(@RequestBody UserUpdateRequest request) {
         userService.updateUser(request);
     }
+
 
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
--- src/main/java/com/group/libraryapp/repository/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.group.libraryapp.repository.user;
 
+import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 public class UserRepository {
 
@@ -8,6 +11,23 @@ public class UserRepository {
 
     public UserRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public void saveUser(String name, Integer age){
+        String sql = "INSERT INTO user(name, age) VALUES(?,?)";
+        jdbcTemplate.update(sql, name, age);
+    }
+
+
+    public List<UserResponse> getUsers(){
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
     }
 
 
