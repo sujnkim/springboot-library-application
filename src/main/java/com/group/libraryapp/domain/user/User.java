95f4d9d44cf2ab757e750f4b1c965a7fba0b3faa
sujnkim <blackout3210@naver.com>
2024-05-07 20:33:09 +0900

    feat: User domain JPA 적용
    MySQL테이블과 User 객체 매핑

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,10 +1,20 @@
 package com.group.libraryapp.domain.user;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false, length = 20)
     private String name;
     private Integer age;
 
+    protected User() {
+    }
 
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
