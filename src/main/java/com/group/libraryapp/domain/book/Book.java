922682ddc340e3a9b42d5c1522dc974be77384e5
sujnkim <blackout3210@naver.com>
2024-05-09 14:43:33 +0900

    feat: 도서 등록 API 구현

--- src/main/java/com/group/libraryapp/domain/book/Book.java
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -12,5 +12,17 @@ public class Book {
     @Column(nullable = false)
     private String name;
 
+    protected Book() {
+
+    }
+
+    public Book(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(
+                    String.format("잘못된 name(%s)이 들어왔습니다.", name)
+            );
+        }
+        this.name = name;
+    }
 }
 
